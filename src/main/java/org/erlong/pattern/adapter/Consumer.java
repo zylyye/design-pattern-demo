17f403cee1a95d10aa3ed45c61b11152494fb906
zylyye <zylyye@gmail.com>
2021-08-30 20:05:32 +0800

    feat: 适配器 Adapter

--- src/main/java/org/erlong/pattern/adapter/Consumer.java
+++ src/main/java/org/erlong/pattern/adapter/Consumer.java
@@ -5,8 +5,8 @@ public class Consumer {
     public static void main(String[] args) {
         ThirdDatabase tdb = new ThirdDatabase();
         // 利用适配器做转换
-        ThirdDatabaseAdapter dbAdabter = new ThirdDatabaseAdapter(tdb);
-        saveToDatabase(dbAdabter);
+        ThirdDatabaseAdapter dbAdapter = new ThirdDatabaseAdapter(tdb);
+        saveToDatabase(dbAdapter);
     }
 
     // 向数据库中写入数据
